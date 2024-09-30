45e2dc56259a91ce00af63c2a7305d192189baf6
arthvm <90999445+arthvm@users.noreply.github.com>
2024-09-30 09:51:04 -0300

    feat: add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -18,7 +19,15 @@ export default function RootLayout({
       <body
         className={`antialiased h-screen w-full`}
       >
-        {children}
+        <div className="flex flex-col py-2 items-center bg-slate-950 border-b-2">
+          <nav className="flex items-center gap-4 text-slate-50 py-4 font-bold">
+            <Link href="/">Introdução</Link>
+            <Link href="/impact">Impacto</Link>
+            <Link href="/catastrophe">Catastrofe</Link>
+            <Link href="/calculus">Calculo</Link>
+          </nav>
+          {children}
+        </div>
       </body>
     </html>
   );
