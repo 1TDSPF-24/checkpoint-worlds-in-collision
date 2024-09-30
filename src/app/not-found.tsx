77605fb66973017b552179cfe6c712943ac3bab0
iannrb <ian007953@gmail.com>
2024-09-30 09:11:49 -0300

    fix: fix responsiviness issue on not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
         <div className="h-full w-full flex flex-col justify-center items-center bg-slate-900">
             <Image src={errorPic} alt="Picture of the author" width={500} height={500} />
             <div className="flex flex-col items-center gap-4">
-                <h1 className="text-2xl text-slate-50">Que pena... Parece que essa página não existe!</h1>
+                <h1 className="text-2xl text-slate-300 text-center">Que pena... Parece que essa página não existe!</h1>
                 <Link href="/" className="px-4 py-2 bg-purple-600 rounded-full border-2 border-purple-700 font-bold text-slate-100">Página Principal</Link>
             </div>
         </div>
